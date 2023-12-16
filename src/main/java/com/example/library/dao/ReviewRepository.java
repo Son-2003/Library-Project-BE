bb13a259c0928953229f4f268bb97d32358400a9
Son-2003 <nguyenducson2915@gmail.com>
2023-12-16 23:47:31 +0700

    create new entity Review and modify MyDataConfig

---
+++ src/main/java/com/example/library/dao/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.example.library.dao;
+
+import com.example.library.entity.Book;
+import com.example.library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findReviewByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+
+}
