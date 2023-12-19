ff1c93b22ac9b6697bc544a4f0ff910d2369e288
Son-2003 <nguyenducson2915@gmail.com>
2023-12-19 18:47:26 +0700

    create Renew and Return book method

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.example.library.controller;
 
 import com.example.library.entity.Book;
+import com.example.library.responsemodels.ShelfCurrentLoansResponse;
 import com.example.library.service.BookService;
 import com.example.library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -15,6 +19,12 @@ public class BookController {
     @Autowired
     public  BookController(BookService bookService){
         this.bookService = bookService;
+    }
+
+    @GetMapping("/secure/currentloans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value = "Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoans(userEmail);
     }
 
     @GetMapping("/secure/currentloans/count")
@@ -33,5 +43,17 @@ public class BookController {
     public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
+    }
+
+    @PutMapping("/secure/return")
+    public void returnBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.returnBook(userEmail, bookId);
+    }
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
     }
 }
--- src/main/java/com/example/library/dao/BookRepository.java
+++ src/main/java/com/example/library/dao/BookRepository.java
@@ -4,6 +4,8 @@ import com.example.library.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -11,4 +13,6 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findBookByTitleContaining(@RequestParam("title") String title, Pageable pageable);
     Page<Book> findBookByCategory(@RequestParam("category") String category, Pageable pageable);
+    @Query("select o from Book o where id in :book_ids")
+    List<Book> findBookByBookIds(@Param("book_ids") List<Long> bookIdList);
 }
