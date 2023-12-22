89ca6104b99a11de279a10104b042dcd9384f055
Son-2003 <nguyenducson2915@gmail.com>
2023-12-22 16:11:06 +0700

    https config

--- src/main/java/com/example/library/controller/AdminController.java
+++ src/main/java/com/example/library/controller/AdminController.java
@@ -8,7 +8,7 @@ import com.example.library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://localhost:3000")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
