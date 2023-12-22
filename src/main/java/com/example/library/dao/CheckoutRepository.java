33636e35941e1721b55622a093612e5b2eb7d71b
Son-2003 <nguyenducson2915@gmail.com>
2023-12-22 13:27:53 +0700

    done ChangeQuantity, DeleteBook

--- src/main/java/com/example/library/dao/CheckoutRepository.java
+++ src/main/java/com/example/library/dao/CheckoutRepository.java
@@ -2,10 +2,16 @@ package com.example.library.dao;
 
 import com.example.library.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findCheckoutByUserEmailAndBookId(String userEmail, Long bookId);
     List<Checkout> findBookByUserEmail(String userEmail);
+    @Modifying
+    @Query("delete from Checkout c where c.bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
--- src/main/java/com/example/library/service/AdminService.java
+++ src/main/java/com/example/library/service/AdminService.java
@@ -1,20 +1,28 @@
 package com.example.library.service;
 
 import com.example.library.dao.BookRepository;
+import com.example.library.dao.CheckoutRepository;
+import com.example.library.dao.ReviewRepository;
 import com.example.library.entity.Book;
 import com.example.library.requestmodels.AddBookRequest;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class AdminService {
     private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+    private CheckoutRepository checkoutRepository;
 
     @Autowired
-    public AdminService(BookRepository bookRepository){
+    public AdminService(BookRepository bookRepository, ReviewRepository reviewRepository, CheckoutRepository checkoutRepository){
         this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+        this.checkoutRepository = checkoutRepository;
     }
 
     public void postBook(AddBookRequest addBookRequest){
@@ -27,5 +35,42 @@ public class AdminService {
         book.setCategory(addBookRequest.getCategory());
         book.setImg(addBookRequest.getImg());
         bookRepository.save(book);
+    }
+
+    public void increaseBookQuantity(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(!book.isPresent()){
+            throw new Exception("Book not found");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        book.get().setCopies(book.get().getCopies() + 1);
+
+        bookRepository.save(book.get());
+    }
+
+    public void decreaseBookQuantity(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(!book.isPresent() || book.get().getCopiesAvailable() <= 0 || book.get().getCopies() <= 0){
+            throw new Exception("Book not found or quantity locked");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        book.get().setCopies(book.get().getCopies() - 1);
+
+        bookRepository.save(book.get());
+    }
+
+    public void deleteBook(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+        if(!book.isPresent()){
+            throw new Exception("Book not found or quantity locked");
+        }
+
+        bookRepository.delete(book.get());
+        checkoutRepository.deleteAllByBookId(bookId);
+        reviewRepository.deleteAllByBookId(bookId);
     }
 }
